96217cf22b16c5ae2d1382d89f5e7430791e3649
Chirag Jani <chirag.jani59001@gmail.com>
2024-04-27 20:09:39 +0530

    feat: added sendmail route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-const multer = require("multer");
-const fs = require("fs");
-const path = require("path");
+// const multer = require("multer");
+// const fs = require("fs");
+// const path = require("path");
 const dotenv = require("dotenv");
 
 const app = express();
@@ -24,13 +24,13 @@ dotenv.config();
 // });
 
 // Nodemailer transporter setup
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.FROM_MAIL_ADDRESS, // Your Gmail address
-//     pass: process.env.APP_PASSWORD, // Your Gmail password
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.FROM_MAIL_ADDRESS, // Your Gmail address
+    pass: process.env.APP_PASSWORD, // Your Gmail password
+  },
+});
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -39,48 +39,48 @@ app.get("/", (req, res) => {
 });
 
 // API endpoint to send email
-// app.post("/sendmail", upload.single("file"), (req, res) => {
-//   const { subject, body } = req.body;
-//   const file = req.file;
+app.post("/sendmail", upload.single("file"), (req, res) => {
+  const { subject, body } = req.body;
+  // const file = req.file;
 
-//   if (!file) {
-//     return res.status(400).send("Please upload a file");
-//   }
+  // if (!file) {
+  //   return res.status(400).send("Please upload a file");
+  // }
 
-//   // Rename the file to have a .pdf extension
-//   const oldPath = file.path;
-//   const newPath = path.join(path.dirname(oldPath), file.originalname + ".pdf");
-//   fs.renameSync(oldPath, newPath);
+  // Rename the file to have a .pdf extension
+  // const oldPath = file.path;
+  // const newPath = path.join(path.dirname(oldPath), file.originalname + ".pdf");
+  // fs.renameSync(oldPath, newPath);
 
-//   // Read the PDF file
-//   const pdfAttachment = fs.readFileSync(newPath);
+  // Read the PDF file
+  // const pdfAttachment = fs.readFileSync(newPath);
 
-//   // Email options
-//   const mailOptions = {
-//     from: process.env.FROM_MAIL_ADDRESS, // Sender address
-//     to: process.env.TO_MAIL_ADDRESS, // Receiver address
-//     subject: subject, // Subject line
-//     text: body, // Plain text body
-//     attachments: [
-//       {
-//         filename: file.originalname + ".pdf",
-//         content: pdfAttachment,
-//         contentType: "application/pdf",
-//       },
-//     ],
-//   };
+  // Email options
+  const mailOptions = {
+    from: process.env.FROM_MAIL_ADDRESS, // Sender address
+    to: process.env.TO_MAIL_ADDRESS, // Receiver address
+    subject: subject, // Subject line
+    text: body, // Plain text body
+    // attachments: [
+    //   {
+    //     filename: file.originalname + ".pdf",
+    //     content: pdfAttachment,
+    //     contentType: "application/pdf",
+    //   },
+    // ],
+  };
 
-//   // Sending email
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       console.log(error);
-//       res.status(500).send("Email sending failed");
-//     } else {
-//       console.log("Email sent: " + info.response);
-//       res.status(200).send("Email sent successfully");
-//     }
-//   });
-// });
+  // Sending email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Email sending failed");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
 
 // Start server
 app.listen(port, () => {
